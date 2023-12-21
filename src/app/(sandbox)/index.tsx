cd77aa3d1621e9ca2ee079b99c70b770211fe79f
DiogoVitorino-dev <diogovf90@hotmail.com>
2023-12-21 11:25:43 -0300

    Feat: added keyboard controller to sandbox

--- src/app/(sandbox)/index.tsx
+++ src/app/(sandbox)/index.tsx
@@ -1,28 +1,36 @@
-import { StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 import { useSharedValue } from "react-native-reanimated";
 
-import Joystick from "@/components/Joystick";
-import Player from "@/components/Player";
 import { View } from "@/components/Themed";
+import Joystick, { JoystickData } from "@/controllers/joystick";
+import Keyboard, { KeyboardData } from "@/controllers/keyboard";
+import Player from "@/models/player";
 
 export default function Sandbox() {
   const posPlayer = useSharedValue({ x: 0, y: 0 });
   const anglePlayer = useSharedValue(0);
 
+  const handleOnMoveJoystick = ({ angle, x, y }: JoystickData) => {
+    posPlayer.value = { x, y };
+    anglePlayer.value = angle;
+  };
+
+  const handleOnMoveKeyboard = ({ angle, x, y, dash }: KeyboardData) => {
+    posPlayer.value = { x, y };
+    anglePlayer.value = angle;
+  };
+
   return (
     <View style={styles.container}>
-      <Joystick
-        onMove={({ angle, x, y }) => {
-          posPlayer.value = {
-            x: posPlayer.value.x + x,
-            y: posPlayer.value.y + y,
-          };
-          anglePlayer.value = angle;
-        }}
-        velocity={3}
-        style={{ bottom: 10, left: 35 }}
-      />
-
+      {Platform.OS === "ios" || Platform.OS === "android" ? (
+        <Joystick
+          onMove={handleOnMoveJoystick}
+          velocity={3}
+          style={{ bottom: 10, left: 35 }}
+        />
+      ) : (
+        <Keyboard onMove={handleOnMoveKeyboard} />
+      )}
       <Player position={posPlayer} lookAngle={anglePlayer} />
     </View>
   );
