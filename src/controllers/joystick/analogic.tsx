d80bdf618207511b51b4e3a9ba50adcd366d3b52
DiogoVitorino-dev <diogovf90@hotmail.com>
2023-12-22 14:36:38 -0300

    Refact: controls separated by types

---
+++ src/controllers/joystick/analogic.tsx
@@ -0,0 +1,144 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  interpolate,
+  useAnimatedReaction,
+  withTiming,
+  runOnJS,
+} from "react-native-reanimated";
+
+import { JoystickData, JoystickProp } from ".";
+
+import { AnglesUtils } from "@/scripts/utils/angleUtils";
+
+export default function AnalogicJoystick({
+  size = 100,
+  velocity = 10,
+  onMove,
+  style,
+}: JoystickProp) {
+  const radius = size / 2;
+  const pointerSize = size / 2.5;
+  const pos = useSharedValue({ x: 0, y: 0 });
+
+  const normalizeData = ({ angle, x, y }: JoystickData): JoystickData => {
+    "worklet";
+
+    // adjusting to velocity range
+    x = interpolate(x, [radius * -1, radius], [velocity * -1, velocity]);
+    y = interpolate(y, [radius * -1, radius], [velocity * -1, velocity]);
+
+    return { angle, x, y };
+  };
+
+  const updatePointerPosition = (x: number, y: number) => {
+    "worklet";
+    pos.value = { x, y };
+  };
+
+  const pan = Gesture.Pan()
+    .onBegin(({ x, y }) => {
+      updatePointerPosition(x - radius, y - radius);
+    })
+
+    .onUpdate(({ x, y }) => {
+      const posX = x - radius;
+      const posY = y - radius;
+
+      // Distance from center to the edge of the area
+      const distance = Math.sqrt(Math.pow(posX, 2) + Math.pow(posY, 2));
+
+      // Limits the pointer to the edge of the area
+      if (distance >= radius) {
+        const factor = radius / distance;
+        updatePointerPosition(posX * factor, posY * factor);
+      } else {
+        updatePointerPosition(posX, posY);
+      }
+    })
+
+    .onFinalize(() => {
+      updatePointerPosition(0, 0);
+    })
+    .withTestId("joystickPan");
+
+  const pointerAnimatedStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: withTiming(pos.value.x, {
+          duration: 20,
+          easing: Easing.out(Easing.ease),
+        }),
+      },
+      {
+        translateY: withTiming(pos.value.y, {
+          duration: 20,
+          easing: Easing.out(Easing.ease),
+        }),
+      },
+    ],
+  }));
+
+  useAnimatedReaction(
+    (): JoystickData => {
+      const angle = AnglesUtils.calculateAngle(pos.value.x, pos.value.y);
+      return normalizeData({
+        angle,
+        x: pos.value.x,
+        y: pos.value.y,
+      });
+    },
+    (currentValue, previousValue) => {
+      if (currentValue !== previousValue && onMove) {
+        runOnJS(onMove)(currentValue);
+      }
+    },
+  );
+
+  return (
+    <View style={[styles.container, style]}>
+      <GestureDetector gesture={pan}>
+        <View
+          testID="pan"
+          style={[
+            { width: size, height: size, borderRadius: radius },
+            styles.area,
+          ]}
+        >
+          <Animated.View
+            style={[
+              {
+                width: pointerSize,
+                height: pointerSize,
+                borderRadius: radius,
+              },
+              styles.pointer,
+              pointerAnimatedStyle,
+            ]}
+          />
+        </View>
+      </GestureDetector>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "indigo",
+    position: "absolute",
+  },
+
+  area: {
+    backgroundColor: "tomato",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  pointer: {
+    backgroundColor: "cyan",
+  },
+});
--- src/controllers/joystick/index.tsx
+++ src/controllers/joystick/index.tsx
@@ -1,153 +1,24 @@
 import React from "react";
-import { View, StyleSheet, ViewStyle } from "react-native";
-import { Gesture, GestureDetector } from "react-native-gesture-handler";
-import Animated, {
-  Easing,
-  useAnimatedStyle,
-  useSharedValue,
-  interpolate,
-  useAnimatedReaction,
-  withTiming,
-  runOnJS,
-} from "react-native-reanimated";
+import { ViewStyle } from "react-native";
 
+import AnalogicJoystick from "./analogic";
 import { ControlData } from "../typesControllers";
 
-import { AnglesUtils } from "@/scripts/utils/angleUtils";
+export type ControlTypes = "analogic";
 
 export interface JoystickData extends ControlData {}
 
-interface JoystickProp {
+export interface JoystickProp {
   size?: number;
   velocity?: number;
   onMove?: (data: JoystickData) => void;
   style?: ViewStyle;
 }
 
-export default function Joystick({
-  size = 100,
-  velocity = 10,
-  onMove,
-  style,
-}: JoystickProp) {
-  const radius = size / 2;
-  const pointerSize = size / 2.5;
-  const pos = useSharedValue({ x: 0, y: 0 });
-
-  const normalizeData = ({ angle, x, y }: JoystickData): JoystickData => {
-    "worklet";
-
-    // adjusting to velocity range
-    x = interpolate(x, [radius * -1, radius], [velocity * -1, velocity]);
-    y = interpolate(y, [radius * -1, radius], [velocity * -1, velocity]);
-
-    return { angle, x, y };
-  };
-
-  const updatePointerPosition = (x: number, y: number) => {
-    "worklet";
-    pos.value = { x, y };
-  };
-
-  const pan = Gesture.Pan()
-    .onBegin(({ x, y }) => {
-      updatePointerPosition(x - radius, y - radius);
-    })
-
-    .onUpdate(({ x, y }) => {
-      const posX = x - radius;
-      const posY = y - radius;
-
-      // Distance from center to the edge of the area
-      const distance = Math.sqrt(Math.pow(posX, 2) + Math.pow(posY, 2));
-
-      // Limits the pointer to the edge of the area
-      if (distance >= radius) {
-        const factor = radius / distance;
-        updatePointerPosition(posX * factor, posY * factor);
-      } else {
-        updatePointerPosition(posX, posY);
-      }
-    })
-
-    .onFinalize(() => {
-      updatePointerPosition(0, 0);
-    })
-    .withTestId("joystickPan");
-
-  const pointerAnimatedStyle = useAnimatedStyle(() => ({
-    transform: [
-      {
-        translateX: withTiming(pos.value.x, {
-          duration: 20,
-          easing: Easing.out(Easing.ease),
-        }),
-      },
-      {
-        translateY: withTiming(pos.value.y, {
-          duration: 20,
-          easing: Easing.out(Easing.ease),
-        }),
-      },
-    ],
-  }));
-
-  useAnimatedReaction(
-    (): JoystickData => {
-      const angle = AnglesUtils.calculateAngle(pos.value.x, pos.value.y);
-      return normalizeData({
-        angle,
-        x: pos.value.x,
-        y: pos.value.y,
-      });
-    },
-    (currentValue, previousValue) => {
-      if (currentValue !== previousValue && onMove) {
-        runOnJS(onMove)(currentValue);
-      }
-    },
-  );
-
-  return (
-    <View style={[styles.container, style]}>
-      <GestureDetector gesture={pan}>
-        <View
-          testID="pan"
-          style={[
-            { width: size, height: size, borderRadius: radius },
-            styles.area,
-          ]}
-        >
-          <Animated.View
-            style={[
-              {
-                width: pointerSize,
-                height: pointerSize,
-                borderRadius: radius,
-              },
-              styles.pointer,
-              pointerAnimatedStyle,
-            ]}
-          />
-        </View>
-      </GestureDetector>
-    </View>
-  );
+interface JoystickControllerProp extends JoystickProp {
+  controlType?: ControlTypes;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "indigo",
-    position: "absolute",
-  },
-
-  area: {
-    backgroundColor: "tomato",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  pointer: {
-    backgroundColor: "cyan",
-  },
-});
+export default function Joystick(props: JoystickControllerProp) {
+  return <AnalogicJoystick {...props} />;
+}
