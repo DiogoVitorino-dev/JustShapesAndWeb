1edaeec81c9e0a110ebe08cbc3fd4b33fbdddcc4
DiogoVitorino-dev <diogovf90@hotmail.com>
2023-12-22 16:42:40 -0300

    Fix: angles Utils supports UI thread calls

--- src/controllers/mobile/analogic.tsx
+++ src/controllers/mobile/analogic.tsx
@@ -64,7 +64,7 @@ export default function AnalogicJoystick({
     .onFinalize(() => {
       updatePointerPosition(0, 0);
     })
-    .withTestId("joystickPan");
+    .withTestId("joystickAnalogicPan");
 
   const pointerAnimatedStyle = useAnimatedStyle(() => ({
     transform: [
--- src/scripts/utils/angleUtils.ts
+++ src/scripts/utils/angleUtils.ts
@@ -1,9 +1,11 @@
 function shiftAngle(angle: number, amount: number) {
+  "worklet";
   if (angle === 0) return 0;
   return (angle + amount) % 360;
 }
 
 function calculateAngle(x: number, y: number) {
+  "worklet";
   let angle = Math.atan2(y, x); // calculating angle
   angle = angle * (180 / Math.PI); // convert radian to degree
   angle = shiftAngle(angle, 360); // convert to positives degrees
