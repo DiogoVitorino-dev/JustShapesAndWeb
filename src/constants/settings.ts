265f14a6067339e8c2ae0ca4899d0f112dec0763
DiogoVitorino-dev <diogovf90@hotmail.com>
2024-01-07 11:55:46 -0300

    Feat: added redux library

---
+++ src/constants/settings.ts
@@ -0,0 +1,13 @@
+// 0 - 100
+export enum DefaultAudioSetting {
+  MUSIC_VOLUME = 80,
+  SOUND_VOLUME = 80,
+}
+
+export enum DefaultKeyboardKeysSettings {
+  UP = "w",
+  LEFT = "a",
+  RIGHT = "d",
+  DOWN = "s",
+  JUMP = " ",
+}
