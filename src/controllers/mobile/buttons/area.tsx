916fe87583198171d7a8fc94798568886f462a59
DiogoVitorino-dev <diogovf90@hotmail.com>
2024-02-07 14:39:44 -0300

    Refact: Improved performance for the Area Button, snapshot file updated

--- src/controllers/mobile/buttons/area.tsx
+++ src/controllers/mobile/buttons/area.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { StyleSheet } from "react-native";
-import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import { GestureResponderEvent, Pressable, StyleSheet } from "react-native";
 import {
   Easing,
   useAnimatedStyle,
@@ -11,76 +10,96 @@ import {
 
 import { ButtonProps } from "..";
 
-import { AnimatedView, View } from "@/components/shared";
+import { AnimatedView } from "@/components/shared";
 import Colors from "@/constants/Colors";
+import { Position } from "@/constants/types";
 
 interface AreaProps extends ButtonProps {
   indicatorSize?: number;
 }
 
-export function AreaButton({ onPress, indicatorSize = 50 }: AreaProps) {
+export function AreaButton({ onPress, indicatorSize = 70 }: AreaProps) {
   const opacityIndicator = useSharedValue(0);
-  const scaleIndicator = useSharedValue(1);
-  const posIndicator = useSharedValue({ x: 0, y: 0 });
+  const scaleIndicator = useSharedValue(0);
+  const posIndicator = useSharedValue<Position>({ x: 0, y: 0 });
 
-  const tap = Gesture.Pan()
-    .maxPointers(1)
-    .onBegin(({ x, y }) => {
-      posIndicator.value = {
-        x: x - indicatorSize / 2,
-        y: y - indicatorSize / 2,
-      };
-    })
-    .onChange(({ x, y }) => {
-      posIndicator.value = {
-        x: x - indicatorSize / 2,
-        y: y - indicatorSize / 2,
-      };
-    })
-    .onTouchesDown(() => {
-      if (onPress) {
-        onPress({ jumping: true });
-      }
+  const callOnPress = (press: boolean) => {
+    "worklet";
+    if (onPress) {
+      onPress({ jumping: press });
+    }
+  };
 
-      opacityIndicator.value = 1;
-      scaleIndicator.value = withRepeat(
-        withTiming(1.5, { duration: 300, easing: Easing.out(Easing.circle) }),
-        -1,
-        true,
-      );
-    })
-    .onTouchesUp(() => {
-      if (onPress) {
-        onPress({ jumping: false });
-      }
-      scaleIndicator.value = 1;
-      opacityIndicator.value = 0;
-    });
+  const updateIndicatorPos = ({ x, y }: Position) => {
+    "worklet";
+    posIndicator.value = {
+      x: x - indicatorSize / 2,
+      y: y - indicatorSize / 2,
+    };
+  };
+
+  const onMove = ({ nativeEvent }: GestureResponderEvent) => {
+    "worklet";
+    updateIndicatorPos({ x: nativeEvent.locationX, y: nativeEvent.locationY });
+  };
+
+  const onPressIn = ({ nativeEvent }: GestureResponderEvent) => {
+    "worklet";
+    updateIndicatorPos({ x: nativeEvent.locationX, y: nativeEvent.locationY });
+
+    opacityIndicator.value = 1;
+    scaleIndicator.value = withRepeat(
+      withTiming(1.3, { duration: 300, easing: Easing.out(Easing.circle) }),
+      -1,
+      true,
+    );
+    callOnPress(true);
+  };
+
+  const onPressOut = () => {
+    "worklet";
+    opacityIndicator.value = 0;
+    scaleIndicator.value = 0;
+    callOnPress(false);
+  };
 
   const indicatorAnimatedStyle = useAnimatedStyle(() => ({
-    opacity: withTiming(opacityIndicator.value, { duration: 100 }),
-    top: withTiming(posIndicator.value.y, { duration: 50 }),
-    left: withTiming(posIndicator.value.x, { duration: 50 }),
-    transform: [{ scale: scaleIndicator.value }],
+    opacity: opacityIndicator.value,
+    transform: [
+      {
+        translateX: posIndicator.value.x,
+      },
+      {
+        translateY: posIndicator.value.y,
+      },
+      {
+        scale: scaleIndicator.value,
+      },
+    ],
   }));
 
   return (
-    <GestureDetector gesture={tap}>
-      <View style={styles.area} transparent>
-        <AnimatedView
-          transparent
-          style={[
-            indicatorAnimatedStyle,
-            styles.indicator,
-            {
-              width: indicatorSize,
-              height: indicatorSize,
-              borderRadius: indicatorSize / 2,
-            },
-          ]}
-        />
-      </View>
-    </GestureDetector>
+    <Pressable
+      onTouchMove={onMove}
+      onPressIn={onPressIn}
+      onPressOut={onPressOut}
+      style={styles.area}
+      testID="pressableArea"
+    >
+      <AnimatedView
+        transparent
+        pointerEvents="none"
+        style={[
+          indicatorAnimatedStyle,
+          styles.indicator,
+          {
+            width: indicatorSize,
+            height: indicatorSize,
+            borderRadius: indicatorSize / 2,
+          },
+        ]}
+      />
+    </Pressable>
   );
 }
 
@@ -88,6 +107,7 @@ const styles = StyleSheet.create({
   area: {
     width: "50%",
     height: "100%",
+    position: "absolute",
   },
   indicator: {
     borderColor: Colors.control.button,
