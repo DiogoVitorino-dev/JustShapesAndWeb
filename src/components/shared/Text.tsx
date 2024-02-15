07af2c2df0fc09cf1c448c8d5b9e8f3ddb7645b6
DiogoVitorino-dev <diogovf90@hotmail.com>
2024-02-15 02:50:13 -0300

    Feat: Added animated version of custom texts

--- src/components/shared/Text.tsx
+++ src/components/shared/Text.tsx
@@ -1,15 +1,37 @@
 import React from "react";
 import { Text as NativeText, StyleSheet, TextProps } from "react-native";
+import Animated, { AnimatedProps } from "react-native-reanimated";
 
 import Colors from "@/constants/Colors";
 
 interface StyledTextProp extends TextProps {}
+
+type Animatable<T extends object> = AnimatedProps<T>;
 
 const DefaultText = ({ children, style, ...others }: StyledTextProp) => (
   <NativeText {...others} style={[styles.default, style]}>
     {children}
   </NativeText>
 );
+
+const DefaultAnimatedText = ({
+  children,
+  style,
+  ...others
+}: Animatable<StyledTextProp>) => (
+  <Animated.Text {...others} style={[styles.default, style]}>
+    {children}
+  </Animated.Text>
+);
+
+const animatedLogo = ({ style, ...others }: Animatable<StyledTextProp>) =>
+  DefaultAnimatedText({ style: [style, styles.fontManjari], ...others });
+
+const animatedTitle = ({ style, ...others }: Animatable<StyledTextProp>) =>
+  DefaultAnimatedText({ style: [style, styles.fontManjari], ...others });
+
+const animatedText = ({ style, ...others }: Animatable<StyledTextProp>) =>
+  DefaultAnimatedText({ style: [style, styles.fontManjari], ...others });
 
 export const TextLogo = ({ style, ...others }: StyledTextProp) =>
   DefaultText({ style: [style, styles.fontMegrim], ...others });
@@ -19,6 +41,12 @@ export const TextTitle = ({ style, ...others }: StyledTextProp) =>
 
 export const Text = ({ style, ...others }: StyledTextProp) =>
   DefaultText({ style: [style, styles.fontManjari], ...others });
+
+export const AnimatedText = {
+  Logo: animatedLogo,
+  Title: animatedTitle,
+  Text: animatedText,
+};
 
 const styles = StyleSheet.create({
   default: {
