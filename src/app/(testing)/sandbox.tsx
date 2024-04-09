2cfc0ba3d6e212af875813f288727659195f38a0
Diogo Vitorino <diogovf90@hotmail.com>
2024-04-09 16:47:19 -0300

    Feat: controllable player script added

--- src/app/(testing)/sandbox.tsx
+++ src/app/(testing)/sandbox.tsx
@@ -1,65 +1,16 @@
 import { useEffect } from "react";
 import { ColorValue, StyleSheet } from "react-native";
-import {
-  useAnimatedStyle,
-  useDerivedValue,
-  useSharedValue,
-} from "react-native-reanimated";
+import { useAnimatedStyle, useSharedValue } from "react-native-reanimated";
 
 import { AnimatedAttacks } from "@/animations/attacks";
 import { AnimatedEffects } from "@/animations/effects";
 import { View } from "@/components/shared";
-import Controller from "@/controllers";
-import { ControlData } from "@/controllers/controllers.type";
 import { useCollisionSystem } from "@/hooks";
 import Circle, { CircleData } from "@/models/geometric/circle";
-import Player, { PlayerData } from "@/models/player";
-import { MovableObject } from "@/scripts/movement/movement.type";
-import useJump from "@/scripts/movement/useJump";
-import { useMovementSystem } from "@/scripts/movement/useMovementSystem";
+import ControllablePlayer from "@/scripts/controllablePlayer";
 
 export default function Sandbox() {
-  const movementPlayer = useSharedValue<MovableObject>({
-    x: 50,
-    y: 50,
-    speedX: 0,
-    speedY: 0,
-    size: 20,
-  });
-
-  const anglePlayer = useSharedValue(0);
-  const jumpPlayer = useSharedValue(false);
-
-  useMovementSystem(movementPlayer);
   const { collided } = useCollisionSystem();
-
-  const handleOnMove = async ({ angle, x, y, jumping }: ControlData) => {
-    "worklet";
-
-    movementPlayer.value = {
-      ...movementPlayer.value,
-
-      speedX: x,
-      speedY: y,
-    };
-    anglePlayer.value = angle;
-    jumpPlayer.value = jumping;
-  };
-
-  const runningJump = useJump(jumpPlayer, movementPlayer);
-
-  const player = useDerivedValue<PlayerData>(
-    () => ({
-      angle: anglePlayer.value,
-      width: 20,
-      height: 20,
-      collidable: { enabled: true, ignore: runningJump.value },
-      x: movementPlayer.value.x,
-      y: movementPlayer.value.y,
-    }),
-    [anglePlayer, movementPlayer, runningJump],
-  );
-
   const circleData: CircleData = {
     x: 200,
     y: 100,
@@ -86,9 +37,7 @@ export default function Sandbox() {
         duration={200}
         delayOfReps={1800}
       >
-        <AnimatedEffects.Shake start={collided} style={[styles.container]}>
-          <Player data={player} />
-
+        <AnimatedEffects.Shake start={false} style={[styles.container]}>
           <AnimatedAttacks.RectangleSmash start />
           <AnimatedAttacks.Grenade
             x={500}
@@ -109,10 +58,10 @@ export default function Sandbox() {
             start
           />
 
-          <Circle data={circleData} style={[backgroundStyle]} />
+          <Circle data={circleData} style={backgroundStyle} />
         </AnimatedEffects.Shake>
 
-        <Controller onMove={handleOnMove} velocity={2} />
+        <ControllablePlayer />
       </AnimatedEffects.Flash>
     </View>
   );
