f49f605049098b3b7bd8970a2ada42741693b1c3
Diogo Vitorino <diogovf90@hotmail.com>
2024-04-26 17:51:30 -0300

    Feat: added collision test for circle model

--- src/__tests__/models/circle.test.tsx
+++ src/__tests__/models/circle.test.tsx
@@ -1,8 +1,15 @@
-import { render, renderHook } from "@testing-library/react-native";
+import {
+  act,
+  render,
+  renderHook,
+  waitFor,
+} from "@testing-library/react-native";
 import { useSharedValue } from "react-native-reanimated";
 import { getAnimatedStyle } from "react-native-reanimated/src/reanimated2/jestUtils";
+import { CrashTestObjectData, CrashTestObject } from "test-utils";
 
 import Circle, { CircleData } from "@/models/geometric/circle";
+import CollisionSystemProvider from "@/scripts/collision/collisionSystemProvider";
 
 describe("Circle - Model tests", () => {
   beforeEach(() => {
@@ -46,5 +53,40 @@ describe("Circle - Model tests", () => {
 
     expect(style.width).toStrictEqual(90);
     expect(style.height).toStrictEqual(90);
+  });
+
+  it.each([
+    ["Should collide", 200],
+    ["Should not collide", 100],
+  ])("%s", async (_, x) => {
+    const dataTestObject: CrashTestObjectData = { x: 300 };
+    const data = renderHook(() =>
+      useSharedValue<CircleData>({
+        diameter: 100,
+        x: 0,
+        y: 0,
+        collidable: { enabled: true },
+      }),
+    );
+
+    const { getByText } = render(
+      <CollisionSystemProvider>
+        <CrashTestObject data={dataTestObject} />
+        <Circle data={data.result.current} collisionMode="target" />
+      </CollisionSystemProvider>,
+    );
+
+    await waitFor(() =>
+      act(() => {
+        data.result.current.value = { ...data.result.current.value, x };
+        jest.advanceTimersByTime(100);
+      }),
+    );
+
+    if (x >= 200) {
+      expect(getByText("collided"));
+    } else {
+      expect(getByText("don't collided"));
+    }
   });
 });
