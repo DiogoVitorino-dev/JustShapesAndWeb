b7fc0d19471d1be10dd0be3aa7390c8533b99fa1
Diogo Vitorino <diogovf90@hotmail.com>
2024-04-26 17:52:09 -0300

    Feat: added collision test for rectangle model

--- src/__tests__/models/rectangle.test.tsx
+++ src/__tests__/models/rectangle.test.tsx
@@ -1,8 +1,15 @@
-import { render, renderHook } from "@testing-library/react-native";
+import {
+  act,
+  render,
+  renderHook,
+  waitFor,
+} from "@testing-library/react-native";
 import { useSharedValue } from "react-native-reanimated";
 import { getAnimatedStyle } from "react-native-reanimated/src/reanimated2/jestUtils";
+import { CrashTestObjectData, CrashTestObject } from "test-utils";
 
 import Rectangle, { RectangleData } from "@/models/geometric/rectangle";
+import CollisionSystemProvider from "@/scripts/collision/collisionSystemProvider";
 
 describe("Rectangle - Model tests", () => {
   beforeEach(() => {
@@ -60,5 +67,41 @@ describe("Rectangle - Model tests", () => {
 
     expect(style.width).toBe(100);
     expect(style.height).toBe(150);
+  });
+
+  it.each([
+    ["Should collide", 200],
+    ["Should not collide", 100],
+  ])("%s", async (_, x) => {
+    const dataTestObject: CrashTestObjectData = { x: 300 };
+    const data = renderHook(() =>
+      useSharedValue<RectangleData>({
+        width: 100,
+        height: 100,
+        x: 0,
+        y: 0,
+        collidable: { enabled: true },
+      }),
+    );
+
+    const { getByText } = render(
+      <CollisionSystemProvider>
+        <CrashTestObject data={dataTestObject} />
+        <Rectangle data={data.result.current} collisionMode="target" />
+      </CollisionSystemProvider>,
+    );
+
+    await waitFor(() =>
+      act(() => {
+        data.result.current.value = { ...data.result.current.value, x };
+        jest.advanceTimersByTime(100);
+      }),
+    );
+
+    if (x >= 200) {
+      expect(getByText("collided"));
+    } else {
+      expect(getByText("don't collided"));
+    }
   });
 });
