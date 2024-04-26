602b689f70a40a698759f790c664d8df66252857
Diogo Vitorino <diogovf90@hotmail.com>
2024-04-26 17:26:58 -0300

    Feat: added collision test for player

--- src/__tests__/models/player.test.tsx
+++ src/__tests__/models/player.test.tsx
@@ -1,8 +1,11 @@
 import { act, render, renderHook } from "@testing-library/react-native";
 import { useSharedValue } from "react-native-reanimated";
 import { getAnimatedStyle } from "react-native-reanimated/src/reanimated2/jestUtils";
+import { CrashTestObject, CrashTestObjectData, Wrapper } from "test-utils";
 
+import { useCollisionSystem } from "@/hooks";
 import Player, { PlayerData } from "@/models/player";
+import CollisionSystemProvider from "@/scripts/collision/collisionSystemProvider";
 
 describe("Player - Model tests", () => {
   beforeEach(() => {
@@ -27,7 +30,7 @@ describe("Player - Model tests", () => {
     data.result.current.value = { x: 50, y: 100, angle: 90 };
 
     act(() => {
-    jest.advanceTimersByTime(1000);
+      jest.advanceTimersByTime(1000);
     });
 
     expect(view).toHaveAnimatedStyle({
@@ -43,7 +46,7 @@ describe("Player - Model tests", () => {
     data.result.current.value = { angle: 90 };
 
     act(() => {
-    jest.advanceTimersByTime(1500);
+      jest.advanceTimersByTime(1500);
     });
 
     const style = getAnimatedStyle(getByTestId("playerModel"));
@@ -60,12 +63,48 @@ describe("Player - Model tests", () => {
     data.result.current.value = { width: 100, height: 150 };
 
     act(() => {
-    jest.advanceTimersByTime(1000);
+      jest.advanceTimersByTime(1000);
     });
 
     const style = getAnimatedStyle(getByTestId("playerModel"));
 
     expect(style.width).toBe(100);
     expect(style.height).toBe(150);
+  });
+
+  it.only.each([
+    ["Should collide", 200],
+    ["Should not collide", 100],
+  ])("%s", async (_, x) => {
+    const dataTestObject: CrashTestObjectData = { x: 300 };
+    const data = renderHook(() =>
+      useSharedValue<PlayerData>({ width: 100, height: 100, x: 0, y: 0 }),
+    );
+
+    const wrapper: Wrapper = ({ children }) => (
+      <CollisionSystemProvider>
+        <CrashTestObject data={dataTestObject} />
+        <Player data={data.result.current} />
+        {children}
+      </CollisionSystemProvider>
+    );
+
+    const system = renderHook(() => useCollisionSystem(), { wrapper });
+
+    act(() => {
+      data.result.current.value = { ...data.result.current.value, x };
+      jest.advanceTimersByTime(100);
+    }).then(
+      (value) => {
+        if (x >= 200) {
+          expect(system.result.current.collided).toBeTruthy();
+        } else {
+          expect(system.result.current.collided).toBeFalsy();
+        }
+
+        return value;
+      },
+      (reason) => reason,
+    );
   });
 });
