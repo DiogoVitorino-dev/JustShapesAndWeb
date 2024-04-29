bb268f5949afc07828f20cd59da7f50a30c5784e
Diogo Vitorino <diogovf90@hotmail.com>
2024-04-29 14:34:55 -0300

    Fix:  Reanimated measure cannot be used with Jest.

--- jest.setup.js
+++ jest.setup.js
@@ -10,3 +10,20 @@ jest.mock("@react-native-async-storage/async-storage", () =>
 jest.mock("./src/scripts/movement/useMovementSystem.ts", () => ({
   useMovementSystem: (targets) => ({ MovementResult: targets }),
 }));
+
+jest.mock("react-native-reanimated", () => {
+  const originalModule = jest.requireActual("react-native-reanimated");
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    measure: () => ({
+      x: 100,
+      y: 100,
+      width: 100,
+      height: 100,
+      pageX: 100,
+      pageY: 100,
+    }),
+  };
+});
