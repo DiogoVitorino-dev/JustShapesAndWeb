111b263c3bf68a23b6bbfd4e43462866d0e6eca5
Diogo Vitorino <diogovf90@hotmail.com>
2024-05-08 18:41:12 -0300

    Refact: Stage name text changed to bouncing text comp

--- src/components/game/stageName/index.tsx
+++ src/components/game/stageName/index.tsx
@@ -1,102 +1,65 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { StyleSheet, Text } from "react-native";
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-} from "react-native-reanimated";
+import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
 
-import Character, { CharacterProps } from "./character";
-
-import { AnimatedStyleApp, DisplayOptions } from "@/constants/commonTypes";
+import { BouncingText, BouncingTextProps } from "@/components/shared";
 import { useAppSelector } from "@/hooks";
 import { StageStatus } from "@/store/reducers/stage/stageReducer";
 import { StageSelectors } from "@/store/reducers/stage/stageSelectors";
 import { SubstagesSelectors } from "@/store/reducers/substages/substagesSelectors";
 
-type ForwardCharacterProps = Pick<CharacterProps, "duration" | "onFinish">;
-
-export interface StageNameProps extends ForwardCharacterProps {
-  style?: AnimatedStyleApp;
-}
+export interface StageNameProps extends Omit<BouncingTextProps, "start"> {}
 
 export default function StageName({
+  containerStyle,
   style,
   onFinish,
   ...props
 }: StageNameProps) {
-  const [shouldStart, setShouldStart] = useState(false);
+  const [start, setStart] = useState(false);
 
   const name = useAppSelector(StageSelectors.selectName);
   const status = useAppSelector(StageSelectors.selectStatus);
   const firstSubstage = useAppSelector(SubstagesSelectors.selectFirstSubstage);
   const substage = useAppSelector(StageSelectors.selectSubstage);
 
-  const display = useSharedValue<DisplayOptions>("none");
-
   useEffect(() => {
     if (substage === firstSubstage?.id && status === StageStatus.Idle) {
-      setShouldStart(true);
+      setStart(true);
     }
   }, [firstSubstage, substage, status]);
 
-  useEffect(() => {
-    if (shouldStart) {
-      display.value = "flex";
-    } else {
-      display.value = "none";
-    }
-  }, [shouldStart]);
-
   const handleFinish = () => {
-    if (shouldStart) {
-      setShouldStart(false);
-    }
+    setStart(false);
 
     if (onFinish) {
       onFinish();
     }
   };
 
-  const Characters = useMemo(() => {
-    const AnimatedName: React.JSX.Element[] = [];
-
-    for (let index = 0; index < name.length; index++) {
-      AnimatedName.push(
-        <Character
-          {...props}
-          start={shouldStart}
-          onFinish={index === 0 ? handleFinish : undefined}
-          index={index}
-          key={`letter_${index}`}
-        >
-          {name[index]}
-        </Character>,
-      );
-    }
-
-    return AnimatedName;
-  }, [shouldStart, name]);
-
-  const animatedContainer = useAnimatedStyle(() => ({
-    display: display.value,
-  }));
-
   return (
-    <Animated.View style={[animatedContainer, styles.container, style]}>
-      <Text style={[styles.text]}>{Characters}</Text>
-    </Animated.View>
+    <BouncingText
+      {...props}
+      start={start}
+      style={
+        typeof style === "function"
+          ? (index) => [styles.text, style(index)]
+          : [styles.text, style]
+      }
+      onFinish={handleFinish}
+      containerStyle={[styles.container, containerStyle]}
+    >
+      {name}
+    </BouncingText>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
-    height: "100%",
+    margin: 36,
     position: "absolute",
-    flexDirection: "row",
   },
 
   text: {
-    margin: 36,
+    fontSize: 46,
   },
 });
