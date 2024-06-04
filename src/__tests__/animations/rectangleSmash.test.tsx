566733ad655fe62b80c0e57fbb81b9db3e10da57
Diogo Vitorino <diogovf90@hotmail.com>
2024-06-04 18:10:11 -0300

    Refact: Renamed initialWidth and initialHeight, added attackSpeed, attackDuration, hideDuration and delay for Rectangle smash attack

--- src/__tests__/animations/rectangleSmash.test.tsx
+++ src/__tests__/animations/rectangleSmash.test.tsx
@@ -31,22 +31,20 @@ describe("Rectangle Smash Attack - Animation tests", () => {
       <RectangleSmash prepareDuration={100} start />,
     );
 
-    let style!: Record<string, any>;
-
     await waitFor(() =>
       act(() => {
         jest.advanceTimersByTime(250);
-        style = getAnimatedStyle(getByTestId("rectangleModel"));
       }),
     );
 
+    const style = getAnimatedStyle(getByTestId("rectangleModel"));
     expect(style.width).toBe(window.result.current.width);
   });
 
   it("Should increment the size value according to the quantity and preparation time", async () => {
     const { getByTestId } = render(
       <RectangleSmash
-        initialWidth={100}
+        initialLength={100}
         prepareDuration={100}
         prepareAmount={100}
         start
@@ -55,34 +53,38 @@ describe("Rectangle Smash Attack - Animation tests", () => {
 
     const view = getByTestId("rectangleModel");
 
-    let style!: Record<string, any>;
-
     await act(() => {
       jest.advanceTimersByTime(100);
-      style = getAnimatedStyle(view);
     });
 
-    expect(style.width).toBeCloseTo(200.5, 0);
+    let style = getAnimatedStyle(view);
+    expect(style.width).toBeCloseTo(200, 0);
 
     await act(() => {
       jest.advanceTimersByTime(200);
-      style = getAnimatedStyle(view);
     });
 
+    style = getAnimatedStyle(view);
     expect(style.width).toBe(window.result.current.width);
   });
 
   it("Should increment the size value according to the smash direction", async () => {
-    const { getByTestId } = render(<RectangleSmash smashTo="vertical" start />);
-
-    const view = getByTestId("rectangleModel");
-
-    let style!: Record<string, any>;
+    const { getByTestId } = render(
+      <RectangleSmash
+        attackDuration={100}
+        attackSpeed={100}
+        prepareDuration={100}
+        smashTo="vertical"
+        start
+      />,
+    );
 
     await act(() => {
-      jest.runAllTimers();
-      style = getAnimatedStyle(view);
+      jest.advanceTimersByTime(300);
     });
+
+    const view = getByTestId("rectangleModel");
+    const style = getAnimatedStyle(view);
 
     expect(style.height).toBe(window.result.current.height);
   });
@@ -96,12 +98,14 @@ describe("Rectangle Smash Attack - Animation tests", () => {
     };
     const { getByTestId } = render(
       <RectangleSmash
-        initialWidth={initial.width}
-        initialHeight={initial.height}
+        initialLength={initial.width}
+        size={initial.height}
         x={initial.x}
         y={initial.y}
-        prepareDuration={100}
         prepareAmount={100}
+        attackDuration={100}
+        attackSpeed={100}
+        prepareDuration={100}
         start
       />,
     );
@@ -116,10 +120,10 @@ describe("Rectangle Smash Attack - Animation tests", () => {
     expect(style.top).toBe(initial.y);
 
     await act(() => {
-      jest.runAllTimers();
-      style = getAnimatedStyle(view);
+      jest.advanceTimersByTime(300);
     });
 
+    style = getAnimatedStyle(view);
     expect(style.width).toBe(window.result.current.width);
     expect(style.height).toBe(initial.height);
     expect(style.left).toBe(initial.x);
@@ -137,8 +141,8 @@ describe("Rectangle Smash Attack - Animation tests", () => {
     const wrapper: Wrapper = ({ children }) => (
       <CollisionSystemProvider>
         <RectangleSmash
-          initialWidth={50}
-          initialHeight={500}
+          initialLength={50}
+          size={500}
           prepareAmount={20}
           prepareDuration={50}
           y={0}
@@ -181,5 +185,103 @@ describe("Rectangle Smash Attack - Animation tests", () => {
       },
       (reason) => reason,
     );
+  });
+
+  it("Should perform attack with specified attack speed", async () => {
+    const { getByTestId } = render(
+      <RectangleSmash
+        initialLength={100}
+        prepareDuration={100}
+        prepareAmount={100}
+        attackSpeed={100}
+        start
+      />,
+    );
+
+    const view = getByTestId("rectangleModel");
+
+    await act(() => {
+      jest.advanceTimersByTime(200);
+    });
+
+    const style = getAnimatedStyle(view);
+    expect(style.width).toBeCloseTo(window.result.current.width, 0);
+  });
+
+  it("Should remain visible after attack with specific duration", async () => {
+    const { getByTestId } = render(
+      <RectangleSmash
+        prepareDuration={100}
+        attackSpeed={100}
+        attackDuration={500}
+        start
+      />,
+    );
+
+    const view = getByTestId("rectangleModel");
+
+    await act(() => {
+      jest.advanceTimersByTime(700);
+    });
+
+    let style = getAnimatedStyle(view);
+    expect(style.width).toBeCloseTo(window.result.current.width, 0);
+
+    await act(() => {
+      jest.advanceTimersByTime(100);
+    });
+
+    style = getAnimatedStyle(view);
+    expect(style.width).not.toBeCloseTo(window.result.current.width, 0);
+  });
+
+  it("Should hide the attack with a specific duration", async () => {
+    const { getByTestId } = render(
+      <RectangleSmash
+        prepareDuration={100}
+        attackSpeed={100}
+        attackDuration={100}
+        hideDuration={500}
+        start
+      />,
+    );
+
+    const view = getByTestId("rectangleModel");
+
+    await act(() => {
+      jest.advanceTimersByTime(300 + 499);
+    });
+
+    let style = getAnimatedStyle(view);
+    expect(style.width).not.toBe(0);
+
+    await act(() => {
+      jest.advanceTimersByTime(1);
+    });
+
+    style = getAnimatedStyle(view);
+    expect(style.width).toBe(0);
+  });
+
+  it("Should delay the animation", async () => {
+    const { getByTestId } = render(
+      <RectangleSmash prepareDuration={100} delay={50} start />,
+    );
+
+    const view = getByTestId("rectangleModel");
+
+    await act(() => {
+      jest.advanceTimersByTime(50);
+    });
+
+    let style = getAnimatedStyle(view);
+    expect(style.width).toBe(0);
+
+    await act(() => {
+      jest.advanceTimersByTime(50);
+    });
+
+    style = getAnimatedStyle(view);
+    expect(style.width).not.toBe(0);
   });
 });
--- src/animations/attacks/rectangleSmash/index.tsx
+++ src/animations/attacks/rectangleSmash/index.tsx
@@ -1,137 +1,219 @@
 import { useEffect } from "react";
 import { useWindowDimensions } from "react-native";
-import {
+import Animated, {
   Easing,
-  WithTimingConfig,
+  SharedValue,
+  cancelAnimation,
   runOnJS,
+  useAnimatedStyle,
   useDerivedValue,
   useSharedValue,
+  withDelay,
   withTiming,
 } from "react-native-reanimated";
 
 import { RunnableAnimation } from "@/animations/animations.type";
-import { AnimatedStyleApp, Position } from "@/constants/commonTypes";
+import Colors from "@/constants/Colors";
+import { AnimatedProps, Position } from "@/constants/commonTypes";
 import Rectangle, { RectangleData } from "@/models/geometric/rectangle";
+import { Collidable } from "@/scripts/collision/collisionDetector";
 
-export type SmashDirection = "horizontal" | "vertical";
+export type SmashDirection = "vertical" | "horizontal";
 
 export interface RectangleSmashConfig extends Partial<Position> {
-  initialWidth?: number;
-  initialHeight?: number;
+  /**
+   * @DocMissing
+   */
+  initialLength?: number;
+
+  /**
+   * @DocMissing
+   */
+  size?: number;
+
+  /**
+   * @DocMissing
+   */
   smashTo?: SmashDirection;
+
+  /**
+   * @DocMissing
+   */
   prepareDuration?: number;
+
+  /**
+   * @DocMissing
+   */
   prepareAmount?: number;
+
+  /**
+   * @DocMissing
+   */
+  attackSpeed?: number;
+
+  /**
+   * @DocMissing
+   */
+  attackDuration?: number;
+
+  /**
+   * @DocMissing
+   */
+  hideDuration?: number;
+
+  /**
+   * @DocMissing
+   */
+  delay?: number;
 }
 
 export interface RectangleSmashProps
   extends RectangleSmashConfig,
     RunnableAnimation {
-  style?: AnimatedStyleApp;
+  style?: AnimatedProps<"View">["style"];
 }
 
 export function RectangleSmash({
-  initialWidth = 100,
-  initialHeight = 80,
+  size = 80,
+  initialLength = 0,
   prepareAmount = 70,
   prepareDuration = 2000,
+  attackDuration = 500,
+  attackSpeed = 150,
+  hideDuration = 200,
+  smashTo = "horizontal",
   x = 0,
   y = 0,
-  smashTo = "horizontal",
+  delay = 0,
   onFinish,
   start,
   style,
 }: RectangleSmashProps) {
   const window = useWindowDimensions();
-  const size = useSharedValue({ width: initialWidth, height: initialHeight });
+
+  const width = useSharedValue(smashTo === "horizontal" ? initialLength : size);
+  const height = useSharedValue(smashTo === "vertical" ? initialLength : size);
+
+  const opacity = useSharedValue(0);
+  const opacityIndicator = useSharedValue(0);
+  const collidable = useSharedValue<Collidable["collidable"]>({
+    enabled: true,
+    ignore: true,
+  });
+
   const rect = useDerivedValue<RectangleData>(() => ({
-    ...size.value,
+    width: width.value,
+    height: height.value,
     x,
     y,
-    collidable: { enabled: true },
+    collidable: { ...collidable.value },
   }));
 
-  const prepareTimingConfig: WithTimingConfig = {
-    duration: prepareDuration,
-    easing: Easing.out(Easing.ease),
-  };
-
-  const attackTimingConfig: WithTimingConfig = {
-    duration: 150,
-    easing: Easing.exp,
-  };
-
-  const opacity = useSharedValue(0.5);
-
-  const endAttack = (finished?: boolean) => {
+  const holdAnimation = (dimension: SharedValue<number>) => {
     "worklet";
-    if (finished && onFinish) {
-      runOnJS(onFinish)();
-    }
+    setTimeout(() => {
+      endAnimation(dimension);
+    }, attackDuration);
   };
 
-  const verticalAttack = () => {
-    function attack(ready?: boolean) {
-      "worklet";
-      if (ready) {
-        size.value = withTiming(
-          { width: size.value.width, height: window.height },
-          attackTimingConfig,
-          endAttack,
-        );
-      }
-    }
+  const SMASH = (dimension: SharedValue<number>) => {
+    "worklet";
+    const dimensionLimit =
+      smashTo === "vertical" ? window.height : window.width;
 
-    opacity.value = 1;
-    size.value = withTiming(
+    dimension.value = withTiming(
+      dimensionLimit,
       {
-        width: size.value.width,
-        height: size.value.height + prepareAmount,
+        duration: attackSpeed,
+        easing: Easing.exp,
       },
-      prepareTimingConfig,
-      (finished) => attack(finished),
+      (fin) => (fin ? holdAnimation(dimension) : undefined),
     );
   };
 
-  const horizontalAttack = () => {
-    function attack(ready?: boolean) {
-      "worklet";
-      if (ready) {
-        size.value = withTiming(
-          { width: window.width, height: size.value.height },
-          attackTimingConfig,
-          endAttack,
-        );
-      }
-    }
+  const prepare = (dimension: SharedValue<number>) => {
+    "worklet";
+    collidable.value = { ...collidable.value, ignore: false };
+    if (delay) {
+      opacity.value = withDelay(delay, withTiming(1, { duration: 100 }));
+      opacityIndicator.value = withDelay(
+        delay,
+        withTiming(0.2, { duration: 100 }),
+      );
 
-    opacity.value = 1;
-    size.value = withTiming(
-      {
-        width: size.value.width + prepareAmount,
-        height: size.value.height,
-      },
-      prepareTimingConfig,
-      (finished) => attack(finished),
+      dimension.value = withDelay(
+        delay,
+        withTiming(
+          initialLength + prepareAmount,
+          { duration: prepareDuration, easing: Easing.out(Easing.ease) },
+          (finished) => (finished ? SMASH(dimension) : undefined),
+        ),
+      );
+    } else {
+      opacity.value = withTiming(1, { duration: 100 });
+      opacityIndicator.value = withTiming(0.2, { duration: 100 });
+
+      dimension.value = withTiming(
+        initialLength + prepareAmount,
+        { duration: prepareDuration, easing: Easing.out(Easing.ease) },
+        (finished) => (finished ? SMASH(dimension) : undefined),
+      );
+    }
+  };
+
+  const endAnimation = (dimension: SharedValue<number>) => {
+    "worklet";
+    collidable.value = { ...collidable.value, ignore: true };
+    opacityIndicator.value = 0;
+
+    dimension.value = withTiming(0, {
+      duration: hideDuration,
+      easing: Easing.out(Easing.ease),
+    });
+
+    opacity.value = withTiming(0, { duration: hideDuration }, (fin) =>
+      fin && onFinish ? runOnJS(onFinish)() : undefined,
     );
   };
 
-  const SMASH = () => {
-    switch (smashTo) {
-      case "vertical":
-        verticalAttack();
-        break;
-
-      default:
-        horizontalAttack();
-        break;
-    }
+  const cancel = (dimension: SharedValue<number>) => {
+    "worklet";
+    cancelAnimation(dimension);
+    cancelAnimation(opacity);
+    cancelAnimation(opacityIndicator);
+    endAnimation(dimension);
   };
 
   useEffect(() => {
     if (start) {
-      SMASH();
+      if (smashTo === "horizontal") prepare(width);
+      else prepare(height);
     }
+    return () => {
+      if (start) {
+        if (smashTo === "horizontal") {
+          cancel(width);
+        } else {
+          cancel(height);
+        }
+      }
+    };
   }, [start]);
 
-  return <Rectangle data={rect} style={[{ opacity }, style]} />;
+  const indicatorStyle = useAnimatedStyle(() => ({
+    width: smashTo === "horizontal" ? window.width : size,
+    height: smashTo === "vertical" ? window.height : size,
+    opacity: opacityIndicator.value,
+    backgroundColor: Colors.entity.enemy,
+    position: "absolute",
+    top: y,
+    left: x,
+  }));
+
+  return (
+    <>
+      <Animated.View style={indicatorStyle} />
+      <Rectangle data={rect} style={[{ opacity }, style]} />;
+    </>
+  );
 }
